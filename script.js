45ca86ecf0449b4dbdf9397735975afc4a2e135f
José-Guilherme-20 <jgscosta20@gmail.com>
2023-01-15 11:15:53 -0300

    criando a conexão dos dados no modal antes de inserir ao carrinho

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-pizzaJson.map((item) => {
+pizzaJson.map((item, index) => {
   let pizzaItem = document.querySelector(".models .pizza-item").cloneNode(true);
 
   // preencher as informações em pizzaItem
-
+  pizzaItem.setAttribute("data-key", index);
   pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
   pizzaItem.querySelector(
     ".pizza-item--price"
   ).innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
   pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+    document.querySelector(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    document.querySelector(".pizzaBig img").src = pizzaJson[key].img;
+    document.querySelector(".pizzaInfo .pizzaInfo--desc").innerHTML =
+      pizzaJson[key].description;
+    document.querySelector(".pizzaInfo .pizzaInfo--actualPrice").innerHTML =
+      pizzaJson[key].price;
+
+    document.querySelector(".pizzaWindowArea").style.opacity = 0;
+    document.querySelector(".pizzaWindowArea").style.display = "flex";
+    setTimeout(() => {
+      document.querySelector(".pizzaWindowArea").style.opacity = 1;
+    }, 200);
+  });
 
   document.querySelector(".pizza-area").append(pizzaItem);
 });
